4eba6480835d18222b56bf75e023c4e6dadc2593
RayyanDakhni <dakhnirayyan@gmail.com>
2022-06-06 07:10:04 +0500

    Adding build to github

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl =
-  "https://ec2-15-206-79-118.ap-south-1.compute.amazonaws.com:5000";
+// const apiUrl = "http://3.111.4.60:5000";
+const apiUrl = "http://localhost:5000";
 
 export default apiUrl;
